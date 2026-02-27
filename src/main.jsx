dc4e9c2f021c995570be0b806e29bcb4e74bc982
stonesun <stonesun201710@gmail.com>
2026-02-27 10:21:03 +0800

    feat: PWA化配置

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,11 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+</StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
